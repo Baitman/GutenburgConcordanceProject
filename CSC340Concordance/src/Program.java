f5b459b0826aa4becbcf848cb7e3f30082c229fb
cnmayse <cnmayse@uncg.edu>
2016-02-09 17:17:47 -0500

    Comments added to  Program.java
    Just added comments

--- CSC340Concordance/src/Program.java
+++ CSC340Concordance/src/Program.java
@@ -1,26 +1,65 @@
+/**
+ * This is main java file for the Concordance program. Contains Main().
+ * @author Charles Mayse
+ * 2/9/16
+ */
 
+/**
+ * Import required for reader
+ */
 import java.util.Scanner;
 
 public class Program {
 
+    /**
+     * main Entry point of the program
+     * @param args Not used.
+     */
     public static void main(String[] args) {
+        
+        /**
+         * Required object references
+         */
+        FileIOManager fileIO = null;
+        ConcManager concManager = null;
 
+        /**
+         * String and String array used to hold user input
+         */
         String input;
         String[] inputArr;
 
+        /**
+         * Welcome prompt.
+         */
         System.out.println("Welcome to Concordance");
 
+        /**
+         * This while loop will continue to run until the user types in 'exit'
+         */
         while (true) {
 
+            /**
+             * Scanner object to read from console
+             * after the user enters a command, parse the command using
+             * the space character as a delimiter
+             */
             Scanner scanner = new Scanner(System.in);
             input = scanner.nextLine();
             inputArr = input.trim().split(" ");
 
-            CommandLine.setCommand(inputArr[0]);
+            /**
+             * Commands must follow certain order to maintain program
+             * integrity
+             */
+            CommandLine.setCommand(inputArr, fileIO, concManager);
 
         }
     }
 
+    /**
+     * Enumeration of all of the possible commands
+     */
     private enum Command {
 
         START, SETDIR, FINDBK, LISTBK, LOADBK, MAKECS,
@@ -28,18 +67,36 @@ public class Program {
         QAPPR, QRANK, QDIST, QADJ, INV, EXIT
     }
 
+    /**
+     * Enumeration of the different states the program will be in
+     * SETUP - program is in a state that file directory must be established
+     * FETCH - program is in a state that a file must be loaded
+     * MANIPULATE - program is in final state in which it can perform any command
+     */
     private enum FlowState {
 
         SETUP, FETCH, MANIPULATE
     }
-
+    
+    /**
+     * Static class that manages the command line interface and flow
+     */
     private static class CommandLine {
 
+        /**
+         * Initial states for program flow
+         */
         private static Command command = Command.START;
         private static FlowState flowState = FlowState.SETUP;
 
-        public static void setCommand(String userCommand) {
-            switch (userCommand) {
+        /**
+         * Changes the current command and checks it against the program flow.
+         * 
+         * @param userCommand is the command in inputArr[0] that the user
+         * entered
+         */
+        public static void setCommand(String[] userCommand, FileIOManager fIOMgr, ConcManager cncMgr) {
+            switch (userCommand[0]) {
                 case ("setdir"):
                     command = Command.SETDIR;
                     if(flowStateTransition(command)){
@@ -112,7 +169,14 @@ public class Program {
                     break;
             }
         }
-
+        
+        /**
+         * Checks the current command and verifies that its execution is appropriate
+         * for the current state of the program. Displays an error message.
+         * 
+         * @param c the command being performed
+         * @return true if the command is acceptable, false otherwise
+         */
         private static boolean flowStateTransition(Command c) {
             if (flowState == FlowState.SETUP && c != Command.SETDIR) {
                 System.out.println("Directory isn't set. Please enter a directory path");
