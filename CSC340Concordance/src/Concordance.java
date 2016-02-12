d74ae433f380fbdf8519b3aaf315cca93f6be01b
myengle <mjyengle@uncg.edu>
2016-02-12 18:25:53 -0500

    Update Concordance.java
    Cleaned up code and added comments.

--- CSC340Concordance/src/Concordance.java
+++ CSC340Concordance/src/Concordance.java
@@ -1,14 +1,11 @@
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * Concordance class.
  *
- * @author Charles Mayse
+ * @author Charles Mayse, Matthew Yengle
  */
 public class Concordance implements Serializable {
 
@@ -24,10 +21,8 @@ public class Concordance implements Serializable {
 
     /**
      * Constructor for Concordance class - creates HashMap after receiving a
-     * string, the hash only represents (word,wordCount).
+     * string, the hash represents (String word, Object Word).
      *
-     * TODO This needs updating for line counts and detecting unnecessary words
-     * and characters.
      *
      * @param textFileString the string representation of a Gutenberg book
      */
@@ -42,18 +37,24 @@ public class Concordance implements Serializable {
         textArr = textFileString.split(" ");
         concordance = new HashMap<String, Word>();
         
-        ArrayList<String> duplicateWord = new ArrayList<String>();
+        
         
         /**
-         * For each item in the textArr, the concordance map will be checked to
-         * make sure that there isn't already a key-value pair for that word. If
-         * the word hasn't been made into a key-value pair, then initialize a
-         * key-value pair to (word,1). If there is a key-value pair already,
-         * replace the key-value pair with (word,++wordCount).
+         * For each item in the textArr, the duplicateWord array will be checked to
+         * make sure that a Word object for that string has not been created yet.
+         * If the duplicateWord array does not have an entry for the string then:
+         * 1. an Object of the string will be made and the object will be added to the HashMap
+         * 2. the string will be added to duplicateWord array to check for duplicates of the same string
+         * Regardless:
+         * For each validated string, the occurrence will be incremented, the line number, and word count number
+         * will be updated within the Word object to keep track of that particular string         
          */
-              
-        //Find the end of the preamble
-        //Keep track of the line number
+        
+         ArrayList<String> duplicateWord = new ArrayList<String>(); 
+         
+        /**
+         * Find the end of the preamble while keeping track of the line number
+         */
         int preambleLine = 1;
         for(String preambleString : textArr){
             if(preambleString.equals("***")){
@@ -65,21 +66,33 @@ public class Concordance implements Serializable {
             }
         }
         
+        /**
+         * The string '|' is being used as a line delimiter and
+         * lineCount is being incremented with every occurrence of the delimiter
+         * 
+         * wordCount is incremented for each valid string
+         * 
+         * The for loop starts at the ending line for the preamble and
+         * the loop breaks when it reaches the start of the ending preamble
+         * which is denoted with the string "***"
+         * 
+         * String from the text is cleaned and stored in cleanString so 
+         * the same word will consistently map to the same place in duplicateArray
+         */
         
         int lineCount = 0;
-        int wordCount =0;
+        int wordCount = 0;
         String cleanString;
         for(String item : textArr){         
+            
             if(item.equals("|")){
-                lineCount++;
+                lineCount++;                
             }            
-            //Start at the end of preamble
+            
             if(lineCount < preambleLine){
                 continue;
             }
-            
-            
-            //break loop at the start of the ending preamble
+                        
             if(item.equals("***")){
                 break;
             }
@@ -120,227 +133,4 @@ public class Concordance implements Serializable {
         return this.concordance;
     }
 }
-    /**
-     * This method needs to be implemented, simply returns the string passed to
-     * it. TODO Implement method.
-     *
-     * @param s Text to remove preamble.
-     * @return
-     */
-   /* private Integer lineStartOfPreamble(String[] s) {
-        int i =0;
-        
-        for(String item : s){
-            if(item.equals("***"))
-            {
-                i++;
-                break;
-            }
-            i++;
-        }
-        
-        return i;
-    }*/
-
-    /**
-     * This method will be used for future implementations of concordance
-     * 
-     * This is the word Object class to be used in the HashMap
-     * It will hold the string word, number of occurrences of the word, 
-     * and the lines the word appears on
-     */
-    /*public class Word implements Serializable {
-
-        private String string;
-        private int occurrenceCount = 0;   
-        private ArrayList<Integer> listOfLines = new ArrayList<Integer>();
-                
-        public Word(String s) {
-            this.string = s;
-        }
-
-        public void setString(String s) {
-            this.string = s;
-        }
-
-        public String getString() {
-            return this.string;
-        }
-        
-        public void incOccurrence(){
-            occurrenceCount++;
-           // System.out.println("String: " + getString() + " Occurrence: " + occurrenceCount);
-        }
-        
-        public Integer getOccurrence(){
-            return occurrenceCount;
-        }
-        
-        public void addNewLine(int newLine){
-            listOfLines.add(newLine);
-        }
-        
-        public ArrayList<Integer> getListOfLines(){
-            return listOfLines;
-        }
-    }
-
-    /**
-     * This method will be used for future implementations of concordance
-     */
-    /*private class Line implements Serializable {
-
-        private int location;
-
-        public void setLine(int i) {
-            this.location = i;
-        }
-
-        public int getLine() {
-            return this.location;
-        }
-    }
-}*/
-    
-//<editor-fold>
-//    /**
-//     * This main needs to be deleted
-//     * @param args 
-//     */
-//    public static void main(String[] args){
-//        TitleElement head = new TitleElement();
-//        TitleElement tail = new TitleElement();
-//        TitleElement curr = new TitleElement();
-//        
-//        
-//    }
-//    
-//    public void addTitle(TitleElement curr, String title){
-//        curr.setNext(curr);
-//        curr = curr.getNext();
-//        curr.setTitle(title);
-//        curr.setText();//pass param to setter
-//        curr.createConcordance();
-//        
-//    }
-//}
-//
-//class TitleElement{
-//    private TitleElement next = new TitleElement();
-//    private String text;
-//    private String title;
-//    private ConcordanceElement[] concordance = new ConcordanceElement[100000];
-//    
-//    public void setNext(TitleElement nxt){
-//        this.next = nxt;
-//    }
-//    
-//    public TitleElement getNext(){
-//        return this.next;
-//    }
-//    
-//    public void setTitle(String textTitle){
-//        this.title = textTitle;
-//    }
-//    
-//    public String getTitle(){
-//        return this.title;
-//    }
-//    
-//    public void setText(){
-//        //text reader
-//    }
-//    
-//    public void createConcordance(){
-//        //Concordance creator based off text, creates ConcordanceElements for words
-//    }
-//}
-//
-//class ConcordanceElement{
-//    private int occurances = 0;
-//    private LineOccurElement[] arr = new LineOccurElement[100000];
-//    
-//    public void intOccurances(){
-//        this.occurances++;
-//    }
-//    
-//    public int getOccurances(){
-//        return this.occurances;
-//    }
-//    public void setPrev(int i, String prev){
-//        arr[i].setPrev(prev);
-//    }
-//    
-//    public String getPrev(int i){
-//        return arr[i].getPrev();
-//    }
-//    
-//    public void setNext(int i, String next){
-//        arr[i].setNext(next);
-//    }
-//    
-//    public String getNext(int i){
-//        return arr[i].getNext();
-//    }
-//    
-//    public void sOL(int i){
-//        arr[i].sOL();
-//    }
-//    
-//    public void eOL(int i){
-//        arr[i].eOL();
-//    }
-//    
-//    public int sOE(int i){
-//        return arr[i].startOrEnd();
-//    }
-//    
-//    
-//    
-//}
-//
-//class LineOccurElement{
-//    private String prev = "";
-//    private String next = "";
-//    private int occurances = 0;
-//    private boolean startOfLine = false;
-//    private boolean endOfLine = false;
-//    
-//    public void incOccurances(){
-//        this.occurances++;
-//    }
-//    public int getOccurances(){
-//        return this.occurances;
-//    }
-//    
-//    public void setPrev(String last){
-//        this.prev = last;
-//    }
-//    
-//    public String getPrev(){
-//        return this.prev;
-//    }
-//    
-//    public void setNext(String following){
-//        this.next = following;
-//    }
-//    
-//    public String getNext(){
-//        return this.next;
-//    }
-//    
-//    public void sOL(){
-//        this.startOfLine = true;
-//    }
-//    
-//    public void eOL(){
-//        this.endOfLine = true;
-//    }
-//    
-//    public int startOrEnd(){
-//        if(this.startOfLine == true) return 1;
-//        if(this.endOfLine == true) return 2;
-//        else return 0;
-//    }
-//}
-//</editor-fold>    
+  
